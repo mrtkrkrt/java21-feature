03b41e2c35c6ec270ec0731ebe0b6ed18634d716
mrtkrkrt <muratkrkrt0711@gmail.com>
2023-10-09 23:39:40 +0300

    cirtual threads

--- src/main/java/org/example/virtualThreads/VirtualThreads.java
+++ src/main/java/org/example/virtualThreads/VirtualThreads.java
@@ -6,16 +6,17 @@ import java.util.concurrent.Executors;
 public class VirtualThreads {
 
     public static void main(String[] args) {
+        Runnable task = () -> {
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Hello Folksdev");
+        };
 
         try(ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor();) {
-            executorService.execute(() -> {
-                try {
-                    Thread.sleep(10000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                System.out.println("Hello Folksdev");
-            });
+            executorService.execute(task);
         }catch (Exception e){
             e.printStackTrace();
         }
